589dcdd55f3454024b6644367b8b82a8317bb1fa
mcristhian <thematheuscris@gmail.com>
2022-11-14 23:25:10 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import Axios from 'axios';
+
+// export default function() {
+//   Axios.get('http://localhost:3000/planos/1')
+//   .then((res) => {
+//     console.log(res)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
+// }
+
+// export default function() {
+//   Axios.post('http://localhost:3000/planos', {
+//     name: 'Test',
+//     description: 'Test',
+//     price: 'Test'
+//   })
+// }
+
+export default function() {
+  Axios.put('http://localhost:3000/planos/6', {
+    name: 'Novo plano',
+    description: 'Plano número 6',
+    price: 'R$ 1.000,00'
+  })
+}
+
+// export default function() {
+//   Axios.delete('http://localhost:3000/planos/5')
+//   .then((res) => {
+//     console.log('Deletado.')
+//   })
+//   .catch((error) => {
+//     console.log('Erro.')
+//   })
+// }
