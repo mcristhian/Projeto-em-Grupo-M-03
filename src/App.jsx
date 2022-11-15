47aeaca57c47430b0fd7d67bb2a5c8daa3f380b8
mcristhian <thematheuscris@gmail.com>
2022-11-14 23:36:35 -0300

    Teste de clonagem do repositório

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Axios from 'axios';
 
-// export default function() {
-//   Axios.get('http://localhost:3000/planos/1')
-//   .then((res) => {
-//     console.log(res)
-//   })
-//   .catch((error) => {
-//     console.log(error)
-//   })
-// }
+export default function() {
+  Axios.get('http://localhost:3000/planos/')
+  .then((res) => {
+    console.log(res)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+}
 
 // export default function() {
 //   Axios.post('http://localhost:3000/planos', {
@@ -18,13 +18,13 @@ import Axios from 'axios';
 //   })
 // }
 
-export default function() {
-  Axios.put('http://localhost:3000/planos/6', {
-    name: 'Novo plano',
-    description: 'Plano número 6',
-    price: 'R$ 1.000,00'
-  })
-}
+// export default function() {
+//   Axios.put('http://localhost:3000/planos/6', {
+//     name: 'Novo plano',
+//     description: 'Plano número 6',
+//     price: 'R$ 1.000,00'
+//   })
+// }
 
 // export default function() {
 //   Axios.delete('http://localhost:3000/planos/5')
