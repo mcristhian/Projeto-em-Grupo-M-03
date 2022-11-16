a2651a5a291f35d07b7da7c90f665537db90063e
mcristhian <thematheuscris@gmail.com>
2022-11-16 09:46:38 -0300

    Atualização db

---
+++ index.js
@@ -0,0 +1,5 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
