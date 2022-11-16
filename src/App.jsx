202659c301b28b4aee3e6cfee14821ce3525dd6d
mcristhian <thematheuscris@gmail.com>
2022-11-16 10:47:35 -0300

    Atualização db

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import Axios from 'axios';
 
+// export default function() {
+//   Axios.get('https://projetoemgrupom03.herokuapp.com/planos/')
+//   .then((res) => {
+//     console.log(res)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
+// }
+
 export default function() {
-  Axios.get('http://localhost:3000/planos/')
-  .then((res) => {
-    console.log(res)
-  })
-  .catch((error) => {
-    console.log(error)
+  Axios.post('https://projetoemgrupom03.herokuapp.com/planos', {
+    name: 'Test',
+    description: 'Test',
+    price: 'Test'
   })
 }
 
 // export default function() {
-//   Axios.post('http://localhost:3000/planos', {
-//     name: 'Test',
-//     description: 'Test',
-//     price: 'Test'
-//   })
-// }
-
-// export default function() {
-//   Axios.put('http://localhost:3000/planos/6', {
+//   Axios.put('https://projetoemgrupom03.herokuapp.com/planos/6', {
 //     name: 'Novo plano',
 //     description: 'Plano número 6',
 //     price: 'R$ 1.000,00'
@@ -27,7 +27,7 @@ export default function() {
 // }
 
 // export default function() {
-//   Axios.delete('http://localhost:3000/planos/5')
+//   Axios.delete('https://projetoemgrupom03.herokuapp.com/planos/5')
 //   .then((res) => {
 //     console.log('Deletado.')
 //   })
