18fec28f73a0e68572be4d695349eaf372e57fa2
mcristhian <thematheuscris@gmail.com>
2022-11-18 19:37:32 -0300

    Adição dos links e mudanças no CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import './App.css';
 import Create from './components/create';
 import Read from './components/read';
 import Update from './components/update';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div className="main">
         <h2 className="main-header">Transportadora</h2>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/read'>Visualizar, atualizar e excluir rotas</Link>
+            </li>
+            <li>
+              <Link to='create'>Adicionar rotas</Link>
+            </li>
+            <li>
+              <Link to='/'>Página inicial</Link>
+            </li>
+          </ul>
+        </nav>
+        <p></p>
         <div>
           <Routes>
           <Route path="/create" element={<Create />} > </Route>
